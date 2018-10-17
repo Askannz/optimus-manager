db2fd640c2d890e6fe7086779f43d4edfda1dc32
Robin Lange <robin.langenc@gmail.com>
2018-10-17 22:17:27 +0800

    [Daemon] Implemented startup switching

--- prime-switcher-daemon.py
+++ prime-switcher-daemon.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
 import os
 import socket
+import envs
+from config import read_startup_mode, write_startup_mode
 from switching import switch_to_intel, switch_to_nvidia
 from bash import exec_bash
-import envs
 
 
 def main():
 
+    # Startup
+    startup_mode = read_startup_mode()
+    print("Startup mode :", startup_mode)
+    if startup_mode == "nvidia_once":
+        write_startup_mode("inactive")  # TEMPORARY : should set next startup to "restore backup" instead
+        switch_to_nvidia()
+    elif startup_mode == "nvidia":
+        switch_to_nvidia()
+    elif startup_mode == "intel":
+        switch_to_intel()
+
+    # UNIX socket
     server = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
     server.bind(envs.SOCKET_PATH)
     os.chmod(envs.SOCKET_PATH, 0o666)
