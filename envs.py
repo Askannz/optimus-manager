6fe9aeedec4cf7f0a01ad933d10a87647cdfb094
Robin Lange <robin.langenc@gmail.com>
2018-11-03 21:21:57 +0800

    Switched default startup mode to intel

--- envs.py
+++ envs.py
@@ -1,6 +1,6 @@
 SOCKET_PATH = "/tmp/prime-switcher"
 STARTUP_MODE_FILE_PATH = "/var/lib/prime_switcher/startup_mode"
-DEFAULT_STARTUP_MODE = "inactive"
+DEFAULT_STARTUP_MODE = "intel"
 SYSTEM_CONFIGS_PATH = "/etc/prime_switcher/configs/"
 XORG_CONF_PATH = "/etc/X11/xorg.conf.d/10-prime-switcher.conf"
 XSETUP_PATH = "/bin/prime_switcher_Xsetup"
