bb66d57b7196f15aefd56877fd32679b2cbcb1b7
Robin Lange <robin.langenc@gmail.com>
2018-11-03 18:14:29 +0800

    Removed leftover backup stuff
    Oopsie

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ def read_startup_mode():
     try:
         with open(envs.STARTUP_MODE_FILE_PATH, 'r') as f:
             content = f.read()
-            if content in ["intel", "nvidia", "inactive", "nvidia_once", "backup"]:
+            if content in ["intel", "nvidia", "inactive", "nvidia_once"]:
                 mode = content
             else:
                 print("WARNING : Invalid startup mode in %s, defaulting to %s." % (envs.STARTUP_MODE_FILE_PATH, envs.DEFAULT_STARTUP_MODE))
@@ -20,7 +20,7 @@ def read_startup_mode():
 
 def write_startup_mode(mode):
 
-    assert mode in ["intel", "nvidia", "inactive", "nvidia_once", "backup"]
+    assert mode in ["intel", "nvidia", "inactive", "nvidia_once"]
 
     with open(envs.STARTUP_MODE_FILE_PATH, 'w') as f:
         f.write(mode)
