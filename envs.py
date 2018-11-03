b28f28e2d87449ceb089ee6d7d01d3ea1ed533fe
Robin Lange <robin.langenc@gmail.com>
2018-11-03 20:40:44 +0800

    Implemented login managers configuration

--- envs.py
+++ envs.py
@@ -4,3 +4,5 @@ DEFAULT_STARTUP_MODE = "inactive"
 SYSTEM_CONFIGS_PATH = "/etc/prime_switcher/configs/"
 XORG_CONF_PATH = "/etc/X11/xorg.conf.d/10-prime-switcher.conf"
 XSETUP_PATH = "/bin/prime_switcher_Xsetup"
+
+SDDM_CONF_NAME = "10-prime-switcher.conf"
