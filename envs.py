da2088854d3638ddc2983723718aeb0589d82401
Robin Lange <robin.langenc@gmail.com>
2018-11-03 18:36:18 +0800

    Better path for startup mode
    According to UNIX standards

--- envs.py
+++ envs.py
@@ -1,5 +1,5 @@
 SOCKET_PATH = "/tmp/prime-switcher"
-STARTUP_MODE_FILE_PATH = "/etc/prime_switcher/startup_mode"  # TODO : put in /var instead and edit it with a daemon command
+STARTUP_MODE_FILE_PATH = "/var/lib/prime_switcher/startup_mode"
 DEFAULT_STARTUP_MODE = "inactive"
 SYSTEM_CONFIGS_PATH = "/etc/prime_switcher/configs/"
 XORG_CONF_PATH = "/etc/X11/xorg.conf.d/10-prime-switcher.conf"
