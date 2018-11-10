36cf2d061462d0fe68920e11f39788a2c15eef66
Robin Lange <robin.langenc@gmail.com>
2018-11-10 18:43:48 +0800

    [Daemon] Fixed wrong path for Xsetup

--- optimus_manager/envs.py
+++ optimus_manager/envs.py
@@ -3,7 +3,7 @@ STARTUP_MODE_FILE_PATH = "/var/lib/optimus-manager/startup_mode"
 DEFAULT_STARTUP_MODE = "intel"
 SYSTEM_CONFIGS_PATH = "/etc/optimus-manager/configs/"
 XORG_CONF_PATH = "/etc/X11/xorg.conf.d/10-optimus-manager.conf"
-XSETUP_PATH = "/bin/optimus-manager_Xsetup"
+XSETUP_PATH = "/usr/bin/optimus-manager_Xsetup"
 
 SDDM_CONF_NAME = "10-optimus-manager.conf"
 
