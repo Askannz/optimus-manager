ec62d74b47068de54e33c858b83652773c0f7afd
Robin Lange <robin.langenc@gmail.com>
2018-11-11 20:28:11 +0800

    [Daemon] Added support for LightDM and GDM
    ...hopefully. I cannot test it right now.

--- optimus_manager/envs.py
+++ optimus_manager/envs.py
@@ -9,6 +9,8 @@ XORG_CONF_PATH = "/etc/X11/xorg.conf.d/10-optimus-manager.conf"
 XSETUP_PATH = "/usr/bin/optimus-manager_Xsetup"
 
 SDDM_CONF_NAME = "10-optimus-manager.conf"
+LIGHTDM_CONF_NAME = "10-optimus-manager.conf"
+GDM_DESKTOP_FILE_NAME = "optimus-manager-xsetup.desktop"
 
 DEFAULT_CONFIG_PATH = "/usr/share/optimus-manager.conf"
 USER_CONFIG_PATH = "/etc/optimus-manager.conf"
