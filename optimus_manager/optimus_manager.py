b2f2703783fef1ebd7d7869a2b4a7a93f0c47f87
Robin Lange <robin.langenc@gmail.com>
2018-11-11 11:37:32 +0800

    [Client] Implemented proper parsing and startup setting

--- optimus_manager/optimus_manager.py
+++ optimus_manager/optimus_manager.py
@@ -1,24 +1,45 @@
 #!/usr/bin/env python
 import sys
+import argparse
 import socket
 import optimus_manager.envs as envs
 
 
-def main():
-
-    if len(sys.argv) < 2:
-        print("Please specify intel or nvidia.")
-        sys.exit(1)
-
-    mode = sys.argv[1]
-    if mode not in ["intel", "nvidia"]:
-        print("Invalid mode selected :", mode)
-        sys.exit(1)
+def send_command(cmd):
 
     client = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
     client.connect(envs.SOCKET_PATH)
-    client.send(mode.encode('utf-8'))
+    client.send(cmd.encode('utf-8'))
     client.close()
+
+
+def main():
+
+    # Arguments parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--switch', metavar='MODE', action='store',
+                        help="Set the GPU mode to MODE (\"intel\" or \"nvidia\") and restart the display manager."
+                             "WARNING : All your applications will close ! Be sure to save your work.")
+    parser.add_argument('--set-startup', metavar='STARTUP_MODE', action='store',
+                        help="Set the startup mode to STARTUP_MODE. Possible modes : "
+                             "intel, nvidia, nvidia_once (starts with Nvidia and reverts to Intel for the next boot")
+    args = parser.parse_args()
+
+    if args.switch:
+
+        if args.switch not in ["intel", "nvidia"]:
+            print("Invalid mode : %s" % args.switch)
+            sys.exit(1)
+
+        send_command(args.switch)
+
+    if args.set_startup:
+
+        if args.set_startup not in ["intel", "nvidia", "nvidia_once"]:
+            print("Invalid startup mode : %s" % args.set_startup)
+            sys.exit(1)
+
+        send_command(args.set_startup)
 
 
 if __name__ == '__main__':
