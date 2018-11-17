9daf8faf6e667dbbb13e5ea13b4464c20658173e
Robin Lange <robin.langenc@gmail.com>
2018-11-17 12:59:24 +0800

    [Daemon] Fixed wrong LightDM config path

--- optimus_manager/login_managers.py
+++ optimus_manager/login_managers.py
@@ -95,7 +95,7 @@ def _configure_sddm(mode):
 
 def _configure_lightdm(mode):
 
-    CONF_FOLDER_PATH = "/etc/lightdm.conf.d/"
+    CONF_FOLDER_PATH = "/etc/lightdm/lightdm.conf.d/"  # /!\ There's one more folder level than SDDM
 
     conf_filepath = os.path.join(CONF_FOLDER_PATH, envs.LIGHTDM_CONF_NAME)
 
