589f52c6332e766fc2111649427f6a91c140b0eb
Trung Dang <bill081001@gmail.com>
2018-11-28 20:02:11 +0700

    Change from non-empty stderr detection to non-zero returncode detection
    This may be a more accurate error checking as Askannz pointed out

--- optimus_manager/bash.py
+++ optimus_manager/bash.py
@@ -8,7 +8,7 @@ class BashError(Exception):
 def exec_bash(command):
     ret = subprocess.run(["bash", "-c", command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    if ret.stderr.decode('utf-8')[:-1] != "":
+    if ret.returncode != 0:
         raise BashError("Failed to execute '%s' : %s" % (command, ret.stderr.decode('utf-8')[:-1]))
 
     return ret
