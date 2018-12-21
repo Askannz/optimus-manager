82312db937bda0f39f2779e9e052769bbcd39649
Trung Dang <bill081001@gmail.com>
2018-12-21 20:23:51 +0700

    Make use of loginctl's terminate-user instead of Xorg killings
    Firstly, this safely remove the current loginctl session, which the previous method of Xorg killings couldn't do.
    
    Secondly, this safely terminates the user's processes before abruptly killing more "stubborn" processes such as X servers. This may be a safer approach comparing to killing the X servers only.

--- optimus_manager/optimus_manager_setup.py
+++ optimus_manager/optimus_manager_setup.py
@@ -91,9 +91,11 @@ def main():
         print("Cleaning up Optimus configuration")
         clean_all()
 
-        # Kill Xorg servers
-        print("Stopping X servers")
-        exec_bash("for pid in $(pidof Xorg); do kill -9 $pid; done;")
+        # Terminate user processes
+        print("Terminating user processes")
+        exec_bash("for user in $(loginctl list-users --no-legend | awk '{print $2}'); do loginctl terminate-user $user; done;")
+        exec_bash("for user in $(loginctl list-users --no-legend | awk '{print $2}'); do loginctl kill-user $user -s SIGKILL; done;")
+
         stopped = _wait_xorg_stop()
         if not stopped:
             print("Cannot stop X servers !")
