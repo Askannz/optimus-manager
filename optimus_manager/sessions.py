9ffb935c418b71886bb30c8edc894dc8ab781fa5
Robin Lange <robin.langenc@gmail.com>
2019-03-09 18:56:07 +1100

    Session cleanup now uses kill-session and SIGKILL if necessary

--- optimus_manager/sessions.py
+++ optimus_manager/sessions.py
@@ -8,19 +8,28 @@ class SessionError(Exception):
 
 def terminate_current_x11_sessions():
 
-    x11_sessions = get_x11_sessions()
-
-    if len(x11_sessions) > 0:
-        print("%d open sessions found, terminating them manually" % len(x11_sessions))
-
-    for session in x11_sessions:
-        terminate_session(session)
-
     def any_session_left(): return (len(get_x11_sessions()) != 0)
 
-    success = poll_block(any_session_left)
+    x11_sessions = get_x11_sessions()
 
-    if not success:
+    if len(x11_sessions) == 0:
+        return
+
+    for mode in ["terminate", "sigterm", "sigkill"]:
+
+        print("%d open sessions found, terminating them manually with \"%s\"" % (len(x11_sessions), mode))
+
+        for session in x11_sessions:
+            terminate_session(session, mode)
+
+        success = poll_block(any_session_left)
+
+        if success:
+            return
+        else:
+            x11_sessions = get_x11_sessions()
+
+    else:
         raise SessionError("Failed to terminate loginctl x11 sessions")
 
 
@@ -35,10 +44,17 @@ def get_x11_sessions():
     return list(filter(_is_x11_session, sessions))
 
 
-def terminate_session(session):
+def terminate_session(session, mode="terminate"):
+
+    assert mode in ["terminate", "sigterm", "sigkill"]
 
     try:
-        exec_bash("loginctl terminate-session %s" % session)
+        if mode == "terminate":
+            exec_bash("loginctl terminate-session %s" % session)
+        elif mode == "sigterm":
+            exec_bash("loginctl kill-session %s -s SIGTERM" % session)
+        elif mode == "sigkill":
+            exec_bash("loginctl kill-session %s -s SIGKILL" % session)
     except BashError as e:
         raise SessionError("Cannot kill session %s : %s" % (session, str(e)))
 
