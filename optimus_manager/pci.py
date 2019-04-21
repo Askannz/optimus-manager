2225c3794832121add06c3ff3b905839cf6521f0
Robin Lange <robin.langenc@gmail.com>
2019-04-21 19:42:08 +1000

    No-DM-restart : Cleanup of obsolete stuff

--- optimus_manager/pci.py
+++ optimus_manager/pci.py
@@ -12,20 +12,6 @@ def set_power_management(enabled):
         _set_mode("on")
 
 
-def reset_gpu():
-
-    bus_ids = get_bus_ids(notation_fix=False)
-    pci_path = "/sys/bus/pci/devices/0000:%s/reset" % bus_ids["nvidia"]
-
-    try:
-        with open(pci_path, "w") as f:
-            f.write("1")
-    except FileNotFoundError:
-        raise PCIError("Cannot find Nvidia PCI path at %s" % pci_path)
-    except IOError:
-        raise PCIError("Error writing to %s" % pci_path)
-
-
 def _set_mode(mode):
 
     bus_ids = get_bus_ids(notation_fix=False)
