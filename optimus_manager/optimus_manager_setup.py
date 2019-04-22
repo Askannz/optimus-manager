de9f6b66bbd97151b547ff5ad8dbd844cafd98c7
Robin Lange <robin.langenc@gmail.com>
2019-04-22 12:12:40 +1000

    No-DM-restart : added first boot setup as an independent "ExecStartPre" script
    Having the first setup be run without synchronization with to display manager
    is a bad bad bad idea. I had already made this mistake for the first version
    of optimus-manager...

--- optimus_manager/optimus_manager_setup.py
+++ optimus_manager/optimus_manager_setup.py
@@ -3,16 +3,17 @@ import sys
 import argparse
 import optimus_manager.envs as envs
 from optimus_manager.config import load_config, ConfigError
-from optimus_manager.var import read_requested_mode, VarError
+from optimus_manager.var import read_startup_mode, read_requested_mode, write_requested_mode, VarError
 from optimus_manager.prime import enable_PRIME
 from optimus_manager.kernel import setup_kernel_state, KernelSetupError
-from optimus_manager.xorg import configure_xorg, XorgSetupError
+from optimus_manager.xorg import configure_xorg, is_xorg_running, XorgSetupError
 
 
 def main():
 
     parser = argparse.ArgumentParser(description="Display Manager setup service for the Optimus Manager tool.\n"
                                                  "https://github.com/Askannz/optimus-manager")
+    parser.add_argument('--setup-boot', action='store_true')
     parser.add_argument('--setup-prime', action='store_true')
     parser.add_argument('--setup-gpu', action='store_true')
 
@@ -23,7 +24,20 @@ def main():
     print("Loading config")
     config = _get_config()
 
-    if args.setup_prime:
+    if args.setup_boot:
+        print("Setting up boot")
+
+        print("Reading startup mode")
+        startup_mode = _get_startup_mode()
+        print("Startup mode is : %s" % startup_mode)
+
+        print("Writing startup mode to requested GPU mode")
+        _write_gpu_mode(config, startup_mode)
+
+        print("Initial GPU setup")
+        _run_initial_gpu_setup(config, startup_mode)
+
+    elif args.setup_prime:
         print("Setting up PRIME")
         enable_PRIME()
 
@@ -43,6 +57,35 @@ def _get_config():
         sys.exit(1)
 
     return config
+
+
+def _get_startup_mode():
+
+    try:
+        startup_mode = read_startup_mode()
+    except VarError as e:
+        print("Cannot read startup mode : %s.\nUsing default startup mode %s instead." % (str(e), envs.DEFAULT_STARTUP_MODE))
+        startup_mode = envs.DEFAULT_STARTUP_MODE
+
+    return startup_mode
+
+
+def _write_gpu_mode(config, mode):
+
+    try:
+        print("Writing requested mode")
+        write_requested_mode(mode)
+
+    except VarError as e:
+        print("Cannot write requested mode : %s" % str(e))
+
+
+def _run_initial_gpu_setup(config, requested_mode):
+
+    if not is_xorg_running():
+        _setup_gpu(config, requested_mode)
+    else:
+        print("Error : the daemon was started while a X server is already running ! Skipping initial GPU setup.")
 
 
 def _get_requested_mode():
