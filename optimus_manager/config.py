88f4cb80aa9c019f969a1347191c883e2d5dce60
Robin Lange <robin.langenc@gmail.com>
2019-04-23 22:10:37 +1000

    Removed parsing of deprecated config path

--- optimus_manager/config.py
+++ optimus_manager/config.py
@@ -10,20 +10,11 @@ class ConfigError(Exception):
 
 def load_config():
 
-    if os.path.isfile(envs.DEPRECATED_USER_CONFIG_PATH):
-        print("Warning : Your configuration file is at the deprecated location %s.\n"
-              "Please move it to %s" % (envs.DEPRECATED_USER_CONFIG_PATH, envs.USER_CONFIG_PATH))
-        user_config_path = envs.DEPRECATED_USER_CONFIG_PATH
-    elif os.path.isfile(envs.USER_CONFIG_PATH):
-        user_config_path = envs.USER_CONFIG_PATH
-    else:
-        user_config_path = None
-
     config = configparser.ConfigParser()
 
     try:
-        if user_config_path is not None:
-            config.read([envs.DEFAULT_CONFIG_PATH, user_config_path])
+        if os.path.isfile(envs.USER_CONFIG_PATH):
+            config.read([envs.DEFAULT_CONFIG_PATH, envs.USER_CONFIG_PATH])
         else:
             config.read(envs.DEFAULT_CONFIG_PATH)
 
