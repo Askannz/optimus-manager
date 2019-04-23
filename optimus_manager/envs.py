b45f59414dc0083a1b82a89ccba3494c433f2040
Robin Lange <robin.langenc@gmail.com>
2019-04-23 22:04:41 +1000

    No-DM-restart : cleaned up some constants

--- optimus_manager/envs.py
+++ optimus_manager/envs.py
@@ -12,13 +12,6 @@ DEFAULT_STARTUP_MODE = "intel"
 SYSTEM_CONFIGS_PATH = "/etc/optimus-manager/configs/"
 XORG_CONF_PATH = "/etc/X11/xorg.conf.d/10-optimus-manager.conf"
 
-PRIME_SETUP_SCRIPT_PATH = "/usr/bin/optimus-manager_prime_setup"
-X_STOP_SCRIPT_PATH = "/usr/bin/optimus-manager_X_stop"
-
-SDDM_CONF_NAME = "10-optimus-manager.conf"
-LIGHTDM_CONF_NAME = "10-optimus-manager.conf"
-GDM_DESKTOP_FILE_NAME = "optimus-manager-xsetup.desktop"
-
 DEFAULT_CONFIG_PATH = "/usr/share/optimus-manager.conf"
 DEPRECATED_USER_CONFIG_PATH = "/etc/optimus-manager.conf"
 USER_CONFIG_PATH = "/etc/optimus-manager/optimus-manager.conf"
