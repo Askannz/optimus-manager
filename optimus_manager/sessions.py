a03e2325e6960b8ce9e359d4b1b58b889a11974f
Robin Lange <robin.langenc@gmail.com>
2019-04-25 14:00:18 +1000

    Also grab user name when listing sessions, and include it in logging

--- optimus_manager/sessions.py
+++ optimus_manager/sessions.py
@@ -17,27 +17,42 @@ def logout_all_x11_sessions():
 
     print("%d open sessions found" % len(x11_sessions))
 
-    for session in x11_sessions:
-        exec_bash("loginctl terminate-session %s" % session)
+    for session_id, user in x11_sessions:
+        print("Terminating session %s (user %s)" % (session_id, user))
+        exec_bash("loginctl terminate-session %s" % session_id)
 
 
 def _get_x11_sessions():
 
     try:
-        sessions_list_str = exec_bash("loginctl list-sessions --no-legend | awk '{print $1}'").stdout.decode('utf-8')[:-1]
+        sessions_list_str = exec_bash("loginctl list-sessions --no-legend").stdout.decode('utf-8')[:-1]
     except BashError as e:
         raise SessionError("Cannot list sessions : %s" % str(e))
 
-    sessions = list(sessions_list_str.splitlines())
-    return list(filter(_is_x11_session, sessions))
+    x11_sessions = []
+
+    for line in sessions_list_str.splitlines():
+
+        line_items = line.split(" ")
+
+        if len(line_items) != 5:
+            print("Error listing X11 sessions : unexpected format at line : %s" % line)
+            continue
+
+        session_id, _, user, _, _ = line_items
+
+        if _is_x11_session(session_id):
+            x11_sessions.append((session_id, user))
+
+    return x11_sessions
 
 
-def _is_x11_session(session):
+def _is_x11_session(session_id):
 
     try:
 
-        session_info = exec_bash("loginctl show-session %s" % session).stdout.decode('utf-8')[:-1]
+        session_info = exec_bash("loginctl show-session %s" % session_id).stdout.decode('utf-8')[:-1]
         return ("Type=x11" in session_info)
 
     except BashError as e:
-        raise SessionError("Error checking type of session %s : %s" % (session, str(e)))
+        raise SessionError("Error checking type of session %s : %s" % (session_id, str(e)))
