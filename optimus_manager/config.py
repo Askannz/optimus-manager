0f13c06dc2cd3b0b763c585b5f41a2f3bb437b0b
Robin Lange <robin.langenc@gmail.com>
2019-08-23 21:16:46 +1000

    Improved config loading with automatic fallback to default values
    The program won't crash anymore if there is an error in the config file.

--- optimus_manager/config.py
+++ optimus_manager/config.py
@@ -1,4 +1,5 @@
 import os
+import copy
 import json
 import configparser
 import optimus_manager.envs as envs
@@ -10,29 +11,35 @@ class ConfigError(Exception):
 
 def load_config():
 
-    config = configparser.ConfigParser()
+    base_config = configparser.ConfigParser()
+    base_config.read(envs.DEFAULT_CONFIG_PATH)
+    validate_config(base_config)
+
+    if not os.path.isfile(envs.USER_CONFIG_PATH):
+        return base_config
 
     try:
-        if os.path.isfile(envs.USER_CONFIG_PATH):
-            config.read([envs.DEFAULT_CONFIG_PATH, envs.USER_CONFIG_PATH])
-        else:
-            config.read(envs.DEFAULT_CONFIG_PATH)
-
+        user_config = configparser.ConfigParser()
+        user_config.read([envs.DEFAULT_CONFIG_PATH, envs.USER_CONFIG_PATH])
     except configparser.ParsingError as e:
-        raise ConfigError("Parsing error : %s" % str(e))
+        print("ERROR : error parsing config file %s. Falling back to default config %s. Error is : %s"
+              % (envs.USER_CONFIG_PATH, envs.DEFAULT_CONFIG_PATH, str(e)))
+        return base_config
 
-    validate_config(config)
+    corrected_config = validate_config(user_config, fallback_config=base_config)
 
-    return config
+    return corrected_config
 
 
-def validate_config(config):
+def validate_config(config, fallback_config=None):
 
     folder_path = os.path.dirname(os.path.abspath(__file__))
     schema_path = os.path.join(folder_path, "config_schema.json")
 
     with open(schema_path, "r") as f:
         schema = json.load(f)
+
+    corrected_config = copy.deepcopy(config)
 
     # Checking if the config file has the required sections and options
     for section in schema.keys():
@@ -45,58 +52,18 @@ def validate_config(config):
             if option not in config[section].keys():
                 raise ConfigError("Cannot find option \"%s\" in section [%s]" % (option, section))
 
-            parameter_type = schema[section][option][0]
+            valid, msg = _validate_option(schema[section][option], config[section][option])
 
-            assert parameter_type in ["multi_words", "single_word", "integer"]
-
-            # Multiple-words parameters
-            if parameter_type == "multi_words":
-                allowed_values = schema[section][option][1]
-                can_be_blank = schema[section][option][2]
-
-                values = config[section][option].replace(" ", "").split(",")
-
-                if values == ['']:
-                    if not can_be_blank:
-                        raise ConfigError("Option \"%s\" in section [%s] requires at least one parameter" % (option, section))
+            if not valid:
+                error_msg = "ERROR : config parsing : error in option \"%s\" in section [%s] : %s" % (option, section, msg)
+                if fallback_config is not None:
+                    print(error_msg)
+                    print("Falling back to default value \"%s\"" % fallback_config[section][option])
+                    corrected_config[section][option] = fallback_config[section][option]
 
                 else:
-                    for val in values:
-                        if val not in allowed_values:
-                            raise ConfigError("Invalid value \"%s\" for option \"%s\" in section [%s]" % (val, option, section))
-
-            # Single-word parameters
-            elif parameter_type == "single_word":
-                allowed_values = schema[section][option][1]
-                can_be_blank = schema[section][option][2]
-
-                val = config[section][option].replace(" ", "")
-
-                if val == "":
-                    if not can_be_blank:
-                        raise ConfigError("Option \"%s\" in section [%s] requires a non-blank value" % (option, section))
-
-                else:
-                    if val not in allowed_values:
-                        raise ConfigError("Invalid value \"%s\" for option \"%s\" in section [%s]" % (val, option, section))
-
-            # Integer parameter
-            elif parameter_type == "integer":
-                can_be_blank = schema[section][option][1]
-
-                val = config[section][option].replace(" ", "")
-
-                if val == "":
-                    if not can_be_blank:
-                        raise ConfigError("Option \"%s\" in section [%s] requires a non-blank integer value" % (option, section))
-
-                else:
-                    try:
-                        v = int(val)
-                        if v <= 0:
-                            raise ValueError
-                    except ValueError:
-                        raise ConfigError("Option \"%s\" in section [%s] requires a non-blank integer value" % (option, section))
+                    raise ConfigError(error_msg)
+            
 
     # Checking if the config file has no unknown section or option
     for section in config.keys():
@@ -105,13 +72,98 @@ def validate_config(config):
             continue
 
         if section not in schema.keys():
-            raise ConfigError("Unknown section %s" % section)
+            print("WARNING : config parsing : unknown section [%s]. Ignoring." % section)
+            continue
 
         for option in config[section].keys():
-
             if option not in schema[section].keys():
-                print("Config parsing : unknown option %s in section %s. Ignoring." % (option, section))
-                del config[section][option]
+                print("WARNING : config parsing : unknown option \"%s\" in section [%s]. Ignoring." % (option, section))
+                del corrected_config[section][option]
+
+    return corrected_config
+
+def _validate_option(schema_option_info, config_option_value):
+
+    parameter_type = schema_option_info[0]
+
+    assert parameter_type in ["multi_words", "single_word", "integer"]
+
+    # Multiple-words parameters
+    if parameter_type == "multi_words":
+        valid, msg = _validate_multi_words(schema_option_info, config_option_value)
+
+    # Single-word parameters
+    elif parameter_type == "single_word":
+        valid, msg = _validate_single_word(schema_option_info, config_option_value)
+
+    # Integer parameter
+    elif parameter_type == "integer":
+        valid, msg = _validate_integer(schema_option_info, config_option_value)
+
+    return valid, msg
+
+
+def _validate_multi_words(schema_option_info, config_option_value):
+
+    parameter_type, allowed_values, can_be_blank = schema_option_info
+    assert parameter_type == "multi_words"
+
+    values = config_option_value.replace(" ", "").split(",")
+
+    if values == ['']:
+        if not can_be_blank:
+            msg = "at least one parameter required"
+            return False, msg
+
+    else:
+        for val in values:
+            if val not in allowed_values:
+                msg = "invalid value \"%s\"" % val
+                return False, msg
+
+    return True, None
+
+def _validate_single_word(schema_option_info, config_option_value):
+
+    parameter_type, allowed_values, can_be_blank = schema_option_info
+    assert parameter_type == "single_word"
+
+    val = config_option_value.replace(" ", "")
+
+    if val == "":
+        if not can_be_blank:
+            msg = "non-blank value required"
+            return False, msg
+
+    else:
+        if val not in allowed_values:
+            msg = "invalid value \"%s\"" % val
+            return False, msg
+
+    return True, None
+
+def _validate_integer(schema_option_info, config_option_value):
+
+    parameter_type, can_be_blank = schema_option_info
+    assert parameter_type == "integer"
+
+    val = config_option_value.replace(" ", "")
+
+    if val == "":
+        if not can_be_blank:
+           msg = "non-blank integer value required"
+           return False, msg
+
+    else:
+        try:
+            v = int(val)
+            if v <= 0:
+                raise ValueError
+        except ValueError:
+            msg = "non-blank integer value required"
+            return False, msg
+
+    return True, None
 
 
 def load_extra_xorg_options():
