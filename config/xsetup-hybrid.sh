7117219790d7195299658f34254aed111391430a
BenKing6 <benking8@hotmail.co.uk>
2020-02-13 19:07:48 +0000

    Added script support for Hybrid mode

---
+++ config/xsetup-hybrid.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Everything you write here will be executed by the display manager when setting up the login screen in Nvidia mode.
+# (but before optimus-manager sets up PRIME with xrandr commands).
