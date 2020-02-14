3a5a218c990cf6eada79bbeebf95715d8b848506
Robin Lange <robin.langenc@gmail.com>
2020-02-15 10:52:05 +1100

    Fixed incorrect info in xsetup-hybrid.sh
    Prime is not set up in hybrid mode, because the laptop screen is
    directly connected to the Intel GPU.

--- config/xsetup-hybrid.sh
+++ config/xsetup-hybrid.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 # Everything you write here will be executed by the display manager when setting up the login screen in Hybrid mode.
-# (but before optimus-manager sets up PRIME with xrandr commands).
