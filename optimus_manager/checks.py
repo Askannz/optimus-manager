71ec8745682d90ec2f4a6b7ebb4fd7993a8e4f66
Hyper-KVM <hyperkvmx86@gmail.com>
2020-02-25 23:49:56 -0500

    add init, elogind, xdm detection functions. Code cleanup.

--- optimus_manager/checks.py
+++ optimus_manager/checks.py
@@ -36,6 +36,7 @@ def is_module_available(module_name):
     else:
         return True
 
+
 def is_module_loaded(module_name):
 
     try:
@@ -45,12 +46,49 @@ def is_module_loaded(module_name):
     else:
         return True
 
+
+def _detect_init_system(init):
+    try:
+        exec_bash("systemctl")
+        return init == "systemd"
+    except BashError:
+        pass
+    try:
+        exec_bash("rc-status")
+        return init == "openrc"
+    except BashError:
+        pass
+    try:
+        exec_bash("sv")
+        return init == "runit"
+    except BashError:
+        pass
+    return False
+
+
 def get_current_display_manager():
 
+    if not _detect_init_system(init="systemd"):
+        return _get_elogind_display_manager()
+    else:
+        pass
+
     if not os.path.isfile("/etc/systemd/system/display-manager.service"):
-        raise CheckError("No display-manager.service file found, ignore this warning if you're not using Systemd.")
+        raise CheckError("No display-manager.service file found")
 
     dm_service_path = os.path.realpath("/etc/systemd/system/display-manager.service")
+    dm_service_filename = os.path.split(dm_service_path)[-1]
+    dm_name = os.path.splitext(dm_service_filename)[0]
+
+    return dm_name
+
+
+def _get_elogind_display_manager():
+
+    if not os.path.isfile("/etc/init.d/xdm"):
+        raise CheckError("No xdm init script fle found")
+
+    dm_service_path = os.path.realpath("/etc/init.d/xdm")
     dm_service_filename = os.path.split(dm_service_path)[-1]
     dm_name = os.path.splitext(dm_service_filename)[0]
 
@@ -63,6 +101,7 @@ def using_patched_GDM():
     folder_path_2 = "/etc/gdm3/Prime"
 
     return os.path.isdir(folder_path_1) or os.path.isdir(folder_path_2)
+
 
 def _is_offloading_available():
 
@@ -88,11 +127,12 @@ def is_login_manager_active():
 
 
 def is_daemon_active():
-    return _is_service_active_bash("optimus-manager")
+    return _is_service_active("optimus-manager")
 
 
 def is_bumblebeed_service_active():
     return _is_service_active("bumblebeed")
+
 
 def _is_gl_provider_nvidia():
 
@@ -109,7 +149,16 @@ def _is_gl_provider_nvidia():
     return False
 
 
+def _is_elogind_present():
+    return os.path.isfile("/usr/lib/libelogind.so")
+
+
 def _is_service_active(service_name):
+
+    if _is_elogind_present():
+        return _is_service_active_bash(service_name)
+    else:
+        pass
 
     try:
         system_bus = dbus.SystemBus()
@@ -117,6 +166,24 @@ def _is_service_active(service_name):
         print("WARNING : Cannot communicate with the DBus system bus to check status of %s."
               " Is DBus running ? Falling back to bash commands" % service_name)
         return _is_service_active_bash(service_name)
+    else:
+        return _is_service_active_dbus(system_bus, service_name)
+
+
+def _is_service_active_dbus(system_bus, service_name):
+
+    systemd = system_bus.get_object("org.freedesktop.systemd1", "/org/freedesktop/systemd1")
+
+    try:
+        unit_path = systemd.GetUnit("%s.service" % service_name, dbus_interface="org.freedesktop.systemd1.Manager")
+    except dbus.exceptions.DBusException:
+        return False
+
+    optimus_manager_interface = system_bus.get_object("org.freedesktop.systemd1", unit_path)
+    properties_manager = dbus.Interface(optimus_manager_interface, 'org.freedesktop.DBus.Properties')
+    state = properties_manager.Get("org.freedesktop.systemd1.Unit", "SubState")
+
+    return state == "running"
 
 
 def _is_service_active_bash(service_name):
