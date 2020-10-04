691f824d6817a495ae2b6f984d6e5a9b6ca7b7d9
Robin Lange <robin.langenc@gmail.com>
2020-10-04 15:56:53 +1100

    Fixed missing amd xorg conf file in PKGBUILD + removed invalid "hybrid" path

--- optimus_manager/envs.py
+++ optimus_manager/envs.py
@@ -22,8 +22,7 @@ USER_CONFIG_PATH = "/etc/optimus-manager/optimus-manager.conf"
 EXTRA_XORG_OPTIONS_PATHS = {
     "intel": "/etc/optimus-manager/xorg-intel.conf",
     "amd": "/etc/optimus-manager/xorg-amd.conf",
-    "nvidia": "/etc/optimus-manager/xorg-nvidia.conf",
-    "hybrid": "/etc/optimus-manager/xorg-hybrid.conf"
+    "nvidia": "/etc/optimus-manager/xorg-nvidia.conf"
 }
 
 XSETUP_SCRIPTS_PATHS = {
