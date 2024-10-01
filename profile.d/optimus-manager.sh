f138a0e6c140fdca3324eae9547dbf55260a0e40
Alberto Salvia Novella <es20490446e@gmail.com>
2024-10-01 14:07:42 +0200

    optimus-manager.sh: correct comments

--- profile.d/optimus-manager.sh
+++ profile.d/optimus-manager.sh
@@ -3,5 +3,6 @@
 if [[ "${XDG_SESSION_TYPE}" == "wayland" ]] && [[ -x "/usr/bin/nvidia-persistenced" ]]; then
 	export __GLX_VENDOR_LIBRARY_NAME="nvidia"
 	export __VK_LAYER_NV_optimus="NVIDIA_only"
-	# The Wayland display manager uses EGL instead, so it will still run on integrated graphics.
+	# The desktop and most apps use EGL instead, so they will still run on the integrated GPU.
+	# Check which apps are using the Nvidia GPU with `nvidia-smi`.
 fi
