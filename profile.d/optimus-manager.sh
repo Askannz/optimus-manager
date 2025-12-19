d3376ef0a2fc2f5a9901790a8ee358125fc63951
Alberto Salvia Novella <es20490446e@gmail.com>
2025-12-19 23:53:11 +0100

    Fix mouse lag on Wayland due to the iGPU being unaware of the dGPU usage

--- profile.d/optimus-manager.sh
+++ profile.d/optimus-manager.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
 if [[ "${XDG_SESSION_TYPE}" == "wayland" ]] && [[ -x "/usr/bin/nvidia-persistenced" ]]; then
+	export __EGL_VENDOR_LIBRARY_FILENAMES="/usr/share/glvnd/egl_vendor.d/50_mesa.json"
 	export __GLX_VENDOR_LIBRARY_NAME="nvidia"
 	export __VK_LAYER_NV_optimus="NVIDIA_only"
-	# The desktop and most apps use EGL instead, so they will still run on the integrated GPU.
-	# Check which apps are using the Nvidia GPU with `nvidia-smi`.
 fi
