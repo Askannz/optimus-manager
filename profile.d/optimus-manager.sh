73a072a29577d94098913e967fd90af5f66458cf
Alberto Salvia Novella <es20490446e@gmail.com>
2026-01-13 04:31:40 +0100

    Let EGL apps decide which GPU to use

--- profile.d/optimus-manager.sh
+++ profile.d/optimus-manager.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 if [[ "${XDG_SESSION_TYPE}" == "wayland" ]] && [[ -x "/usr/bin/nvidia-persistenced" ]]; then
-	export __EGL_VENDOR_LIBRARY_FILENAMES="/usr/share/glvnd/egl_vendor.d/50_mesa.json"
 	export __GLX_VENDOR_LIBRARY_NAME="nvidia"
 	export __VK_LAYER_NV_optimus="NVIDIA_only"
 fi
