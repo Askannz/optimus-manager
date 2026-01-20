2417d13a820ecfa80ff0220b07f5ece43d0efc31
Alberto Salvia Novella <es20490446e@gmail.com>
2026-01-20 12:33:14 +0100

    Let Vulkan apps decide their GPU

--- profile.d/optimus-manager.sh
+++ profile.d/optimus-manager.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-if [[ "${XDG_SESSION_TYPE}" == "wayland" ]] && [[ -x "/usr/bin/nvidia-persistenced" ]]; then
+if [[ "${XDG_SESSION_TYPE}" == "wayland" ]]; then
 	export __GLX_VENDOR_LIBRARY_NAME="nvidia"
-	export __VK_LAYER_NV_optimus="NVIDIA_only"
 fi
