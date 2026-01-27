17298c12ed147d7b4895cd321ad346089fa11652
Alberto Salvia Novella <es20490446e@gmail.com>
2026-01-27 05:56:47 +0100

    By default use the dGPU on Wayland for everything

--- profile.d/optimus-manager.sh
+++ profile.d/optimus-manager.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
+set -e
 
-if [[ "${XDG_SESSION_TYPE}" == "wayland" ]]; then
-	export __GLX_VENDOR_LIBRARY_NAME="nvidia"
+if [[ "${XDG_SESSION_TYPE}" == "wayland" ]] &&
+[[ -z "${__NV_PRIME_RENDER_OFFLOAD}" ]]; then
+	__NV_PRIME_RENDER_OFFLOAD="$(
+		lspci |
+		grep --extended-regexp --ignore-case "vga|3d" |
+		grep --line-number "NVIDIA" |
+		cut --delimiter=":" --fields=1 |
+		head -n1
+	)"
+
+	if [[ -z "${__NV_PRIME_RENDER_OFFLOAD}" ]]; then
+		unset __NV_PRIME_RENDER_OFFLOAD
+	else
+		export __NV_PRIME_RENDER_OFFLOAD
+		export __GLX_VENDOR_LIBRARY_NAME="nvidia"
+		export __VK_LAYER_NV_optimus="NVIDIA_only"
+	fi
 fi
