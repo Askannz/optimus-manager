2a8f1b7f65c01a693f09ea0a85b74265af1e4178
Alberto Salvia Novella <es20490446e@gmail.com>
2026-01-28 01:27:36 +0100

    Offload is a flag, not an index

--- profile.d/optimus-manager.sh
+++ profile.d/optimus-manager.sh
@@ -1,21 +1,8 @@
 #! /bin/bash
 set -e
 
-if [[ "${XDG_SESSION_TYPE}" == "wayland" ]] &&
-[[ -z "${__NV_PRIME_RENDER_OFFLOAD}" ]]; then
-	__NV_PRIME_RENDER_OFFLOAD="$(
-		lspci |
-		grep --extended-regexp --ignore-case "vga|3d" |
-		grep --line-number "NVIDIA" |
-		cut --delimiter=":" --fields=1 |
-		head -n1
-	)"
-
-	if [[ -z "${__NV_PRIME_RENDER_OFFLOAD}" ]]; then
-		unset __NV_PRIME_RENDER_OFFLOAD
-	else
-		export __NV_PRIME_RENDER_OFFLOAD
-		export __GLX_VENDOR_LIBRARY_NAME="nvidia"
-		export __VK_LAYER_NV_optimus="NVIDIA_only"
-	fi
+if [[ "${XDG_SESSION_TYPE}" == "wayland" ]]; then
+	export __NV_PRIME_RENDER_OFFLOAD=1
+	export __GLX_VENDOR_LIBRARY_NAME="nvidia"
+	export __VK_LAYER_NV_optimus="NVIDIA_only"
 fi
